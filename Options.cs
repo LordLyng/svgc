9b90ba252589ac2a40626e42295755dc5810454e
Morten Hau Lyng <mhl@it-minds.dk>
2020-08-14 22:23:36 +0200

    Bumped versions

--- Options.cs
+++ Options.cs
@@ -6,10 +6,10 @@ namespace svgc
 {
     public static class Options
     {
-        public static Option OutputOption => new Option(new[] {"--output", "-o"}) {Argument = new Argument<FileInfo>().LegalFilePathsOnly(), Description = "Output path", Required = false};
-        public static Option ImageTypeOption => new Option(new[] {"--image-type","-it"}) {Argument = Arguments.ImageTypeArgument, Description = "Set the desired type of the output image, defaults to \"png\"", Required = false};
-        public static Option HeightOption => new Option(new[] {"--height", "-h"}) {Argument = Arguments.HeightArgument, Description = "Sets the height in pixels of the resulting image", Required = false};
-        public static Option WidthOpton => new Option(new[] {"--width", "-w"}) {Argument = Arguments.WidthArgument, Description = "Sets the width in pixels of the resulting image", Required = false};
-        public static Option BackgroundOption => new Option(new[] {"--background", "-b"}) {Argument = Arguments.BackgroundArgument, Description = "Sets the background color of the output image (accepts hex colors)", Required = false};
+        public static Option OutputOption => new Option(new[] { "--output", "-o" }) { Argument = new Argument<FileInfo>().LegalFilePathsOnly(), Description = "Output path", IsRequired = false };
+        public static Option ImageTypeOption => new Option(new[] { "--image-type", "-it" }) { Argument = Arguments.ImageTypeArgument, Description = "Set the desired type of the output image, defaults to \"png\"", IsRequired = false };
+        public static Option HeightOption => new Option(new[] { "--height", "-h" }) { Argument = Arguments.HeightArgument, Description = "Sets the height in pixels of the resulting image", IsRequired = false };
+        public static Option WidthOpton => new Option(new[] { "--width", "-w" }) { Argument = Arguments.WidthArgument, Description = "Sets the width in pixels of the resulting image", IsRequired = false };
+        public static Option BackgroundOption => new Option(new[] { "--background", "-b" }) { Argument = Arguments.BackgroundArgument, Description = "Sets the background color of the output image (accepts hex colors)", IsRequired = false };
     }
 }
